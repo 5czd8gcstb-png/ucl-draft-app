aadd0afd112d5aab4ec18a49dabdb565b87796d7
5czd8gcstb-png <5czd8gcstb@privaterelay.appleid.com>
2026-09-15 07:34:29 +0300

    Add UCL Draft app configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+// UCL Draft launcher configuration.
+// Apps Script Web App URL is already configured.
+window.UCL_DRAFT_CONFIG = {
+  appUrl: 'https://script.google.com/macros/s/AKfycbyAbTp43XsUSMQu6fV937iUJ_LLi1PxYxnqG7GxhGKCBOH0xSTcGvVc7ls7GXEKwqI06A/exec'
+};
