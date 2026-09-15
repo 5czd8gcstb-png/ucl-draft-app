d95368576c168a5bd75a6e39eca1b6e86f6af1f8
5czd8gcstb-png <5czd8gcstb@privaterelay.appleid.com>
2026-09-15 07:34:49 +0300

    Add service worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'ucl-draft-shell-v2';
+const SHELL = [
+  './',
+  './index.html',
+  './config.js',
+  './manifest.webmanifest',
+  './icons/apple-touch-icon.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png',
+  './icons/favicon-32.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
